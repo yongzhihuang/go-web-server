ffae2261d10ca544419437bf98462447c1a0d208
Yongzhi Huang <yhuang@fastcompany.com>
2016-07-24 22:41:50 -0400

    err handling, static directory handlers

--- server.go
+++ server.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
+	http.HandleFunc("/", serveTemplate)
 
 	port := ":1337"
 	log.Println("Listening on port %s", port)
 	http.ListenAndServe(port, nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := path.Join("templates", "layout.html")
+	fp := path.Join("templates", r.URL.Path)
+
+	// request template is 404
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// Req is a directory, 404
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
 }
